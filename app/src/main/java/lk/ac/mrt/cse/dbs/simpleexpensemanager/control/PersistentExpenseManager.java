c0d6ca05b5ee255dc689bad593430a1e1149d01d
Budvin Chathura <chathura97rc@gmail.com>
2019-12-19 08:53:46 +0530

    Remove dummy data

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -32,9 +32,9 @@ public class PersistentExpenseManager extends ExpenseManager {
         setAccountsDAO(persistentAccountDAO);
 
         // dummy data
-        Account dummyAcct1 = new Account("12345Azzzz", "Yoda Bank", "Anakin Skywalker", 10000.0);
-        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
-        getAccountsDAO().addAccount(dummyAcct1);
-        getAccountsDAO().addAccount(dummyAcct2);
+//        Account dummyAcct1 = new Account("12345Azzzz", "Yoda Bank", "Anakin Skywalker", 10000.0);
+//        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+//        getAccountsDAO().addAccount(dummyAcct1);
+//        getAccountsDAO().addAccount(dummyAcct2);
     }
 }
